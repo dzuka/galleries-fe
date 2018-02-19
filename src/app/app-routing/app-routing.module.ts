35d2d5ed6375cb978196f0d8dd89c46839c579ce
dzuka <milandzukic@gmail.com>
2018-02-19 21:27:30 +0100

    all components + routing

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from '../app.component';
+import { LayoutComponent } from '../layout/layout.component';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
+import { MyGalleriesComponent } from '../my-galleries/my-galleries.component';
+import { CreateNewGalleryComponent } from '../create-new-gallery/create-new-gallery.component';
+import { AllGalleriesComponent } from '../all-galleries/all-galleries.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: AllGalleriesComponent    
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'my-galleries',
+    component: MyGalleriesComponent
+  },
+  {
+    path: 'create',
+    component: CreateNewGalleryComponent
+  }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes
+    )
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MyGalleriesComponent } from './my-galleries/my-galleries.component';
+import { CreateNewGalleryComponent } from './create-new-gallery/create-new-gallery.component';
+import { AllGalleriesComponent } from './all-galleries/all-galleries.component';
+import { AppRoutingModule } from '../app/app-routing/app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    LoginComponent,
+    RegisterComponent,
+    MyGalleriesComponent,
+    CreateNewGalleryComponent,
+    AllGalleriesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
